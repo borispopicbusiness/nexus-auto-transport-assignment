781ae627e414a225add92dbe4939f4c0d5849a3a
Boris Popic <borispopic.business@gmail.com>
2024-11-24 09:10:34 +0100

    icons improved

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -3,7 +3,7 @@
     <div class="menu-item" @click="navigateTo('/dropdown')">
       <img
           class="icon"
-          src="https://img.icons8.com/?size=100&id=40902&format=png&color=000000"
+          src="https://img.icons8.com/?size=100&id=SPPJjrSE810g&format=png&color=000000"
           alt="Dropdown icon"
       />
       <span class="menu-text">Dropdown Page</span>
@@ -12,7 +12,7 @@
     <div class="menu-item" @click="navigateTo('/editor')">
       <img
           class="icon"
-          src="https://img.icons8.com/?size=100&id=8174&format=png&color=000000"
+          src="https://img.icons8.com/?size=100&id=62186&format=png&color=000000"
           alt="Table icon"
       />
       <span class="menu-text">Model Page</span>
